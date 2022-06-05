6c4aa30964b21fdfe50b74768540ea22bb6716bc
shabnum1 <shabnumhuda7259@gmail.com>
2022-06-05 10:01:25 -0700

    june 3

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,15 @@
-const UserModel= require("../models/userModel")
+const bookModel = require("../models/bookModel")
 
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
-} 
-
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
+const createBook = async function(req, res) {
+    let data = req.body
+    let savedData = await bookModel.create(data)
+    res.send({ msg: savedData })
 }
 
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+const getBooksData = async function(req, res) {
+    let allbooks = await bookModel.find()
+    res.send({ msg: allbooks })
+}
+
+module.exports.createBook = createBook
+module.exports.getBooksData = getBooksData
