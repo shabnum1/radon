b2698194242e1160a585ab069d642273fab46935
shabnum1 <shabnumhuda7259@gmail.com>
2022-06-05 08:40:02 -0700

    june 3

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,7 +4,7 @@ const createUser= async function (req, res) {
     let data= req.body
     let savedData= await UserModel.create(data)
     res.send({msg: savedData})
-}
+} 
 
 const getUsersData= async function (req, res) {
     let allUsers= await UserModel.find()
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://huda123:MaZjaXxcN2lw6iVV@cluster0.je5ld.mongodb.net/test?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
