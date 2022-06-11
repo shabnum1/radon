af858117132e4bb688144ea48f7b50021753fb4d
shabnum1 <shabnumhuda7259@gmail.com>
2022-06-11 11:39:42 -0700

    middleware

---
+++ src/models/publisherModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const publisherSchema = new mongoose.Schema( {
+    
+    publisher_name: String,
+    
+    headQuater :String
+
+}, { timestamps: true });
+
+module.exports = mongoose.model('NewPublisher', publisherSchema)
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,32 +1,15 @@
 const express = require('express');
 const router = express.Router();
-//const NewauthorModel = require("../models/NewauthorModel.js")
-//const NewbookModel = require("../models/NewbookModel.js")
-const bookController = require("../controllers/bookController")
-const authorController = require("../controllers/authorController")
-const publisherController = require("../controllers/publisherController")
-router.get("/test-me", function(req, res) {
+// const UserModel= require("../models/userModel.js")
+const UserController= require("../controllers/userController")
+const BookController= require("../controllers/bookController")
+const commonMW = require ("../middlewares/commonMiddlewares")
+
+router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
-});
+})
 
-router.post("/createAuthor", authorController.createAuthor)
 
-router.post("/createBook", bookController.createBook)
 
-router.post("/createpublisher",publisherController.createPublisher)
 
-router.get("/getBooksWith_A_P_Details", bookController.getBooksWith_A_P_Details) 
-                                                      
-router.get("/getBooksWithAuthorDetails",bookController.getBooksWithAuthorDetails) 
-
-router.get("/getAuthorsData", authorController.getAuthorsData) 
-
-router.get("/getBooksData", bookController.getBooksData) 
-
-router.put("/updateIsHardCover",bookController.updateIsHardCover)
-
-router.put("/authorRating",bookController.authorRating) 
-
-router.get("/bookBetween", bookController.bookBetween)
-
-module.exports = router;
+router.post("/createBook", BookController.createBook  )
