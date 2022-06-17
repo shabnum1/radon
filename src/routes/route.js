b0913b30560dd7d6a7d0ef2b6631eccd19a2e6a8
shabnum1 <shabnumhuda7259@gmail.com>
2022-06-17 05:45:08 -0700

    try catch

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const CowinController= require("../controllers/cowinController")
-
+const WeatherController= require("../controllers/weatherController")
 
 
 router.get("/test-me", function (req, res) {
@@ -12,8 +12,8 @@ router.get("/test-me", function (req, res) {
 router.get("/cowin/states", CowinController.getStates)
 router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
 router.get("/cowin/getByPin", CowinController.getByPin)
-
 router.post("/cowin/getOtp", CowinController.getOtp)
+router.get("/getWeatherOfLondon", WeatherController.getWeatherOfLondon)
 
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
 
